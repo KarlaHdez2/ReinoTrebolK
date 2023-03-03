2b3ca71f4d4907d19dbaa1e7a30b22c9fac6d647
KarlaHdez2 <karlagpe.hdez@gmail.com>
2023-03-02 23:53:25 -0600

    modificacion SL

--- BaseDatos_MySQL/BD_ReinoTrebol.sql
+++ BaseDatos_MySQL/BD_ReinoTrebol.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE  IF NOT EXISTS `reinotrebol` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `reinotrebol3`;
+USE `reinotrebol`;
 -- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: reinotrebol
